b5d6ed57d8c3c0db3db870d37b5cf3bf97befe96
kzorin52 <kzorin52@gmail.com>
2020-06-22 21:44:56 +0600

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,231 @@
+﻿using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace IP_TmaP
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+            string target_name = Path.GetFileNameWithoutExtension(Application.ExecutablePath);
+            System.Diagnostics.Process[] local_procs = System.Diagnostics.Process.GetProcesses();
+            try
+            {
+                System.Diagnostics.Process target_proc = local_procs.First(p => p.ProcessName == target_name);
+                target_proc.Kill();
+            }
+            catch { }
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.TopPanel = new Guna.UI2.WinForms.Guna2Panel();
+            this.MinimizeBoxC = new Guna.UI2.WinForms.Guna2ControlBox();
+            this.MaximizeBoxC = new Guna.UI2.WinForms.Guna2ControlBox();
+            this.TextLabel = new Guna.UI.WinForms.GunaLabel();
+            this.CloseBoxC = new Guna.UI2.WinForms.Guna2ControlBox();
+            this.FormDrag = new Guna.UI2.WinForms.Guna2DragControl(this.components);
+            this.ipBox = new Guna.UI2.WinForms.Guna2TextBox();
+            this.foundBtn = new Guna.UI2.WinForms.Guna2Button();
+            this.gMapControl1 = new GMap.NET.WindowsForms.GMapControl();
+            this.TopPanel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // TopPanel
+            // 
+            this.TopPanel.BackColor = System.Drawing.Color.White;
+            this.TopPanel.Controls.Add(this.MinimizeBoxC);
+            this.TopPanel.Controls.Add(this.MaximizeBoxC);
+            this.TopPanel.Controls.Add(this.TextLabel);
+            this.TopPanel.Controls.Add(this.CloseBoxC);
+            this.TopPanel.Dock = System.Windows.Forms.DockStyle.Top;
+            this.TopPanel.Location = new System.Drawing.Point(0, 0);
+            this.TopPanel.Name = "TopPanel";
+            this.TopPanel.ShadowDecoration.Parent = this.TopPanel;
+            this.TopPanel.Size = new System.Drawing.Size(800, 30);
+            this.TopPanel.TabIndex = 0;
+            // 
+            // MinimizeBoxC
+            // 
+            this.MinimizeBoxC.Animated = true;
+            this.MinimizeBoxC.ControlBoxType = Guna.UI2.WinForms.Enums.ControlBoxType.MinimizeBox;
+            this.MinimizeBoxC.Dock = System.Windows.Forms.DockStyle.Right;
+            this.MinimizeBoxC.FillColor = System.Drawing.Color.White;
+            this.MinimizeBoxC.HoverState.Parent = this.MinimizeBoxC;
+            this.MinimizeBoxC.IconColor = System.Drawing.Color.Black;
+            this.MinimizeBoxC.Location = new System.Drawing.Point(665, 0);
+            this.MinimizeBoxC.Name = "MinimizeBoxC";
+            this.MinimizeBoxC.PressedColor = System.Drawing.Color.Gainsboro;
+            this.MinimizeBoxC.ShadowDecoration.Parent = this.MinimizeBoxC;
+            this.MinimizeBoxC.Size = new System.Drawing.Size(45, 30);
+            this.MinimizeBoxC.TabIndex = 7;
+            // 
+            // MaximizeBoxC
+            // 
+            this.MaximizeBoxC.Animated = true;
+            this.MaximizeBoxC.ControlBoxType = Guna.UI2.WinForms.Enums.ControlBoxType.MaximizeBox;
+            this.MaximizeBoxC.Dock = System.Windows.Forms.DockStyle.Right;
+            this.MaximizeBoxC.FillColor = System.Drawing.Color.White;
+            this.MaximizeBoxC.HoverState.Parent = this.MaximizeBoxC;
+            this.MaximizeBoxC.IconColor = System.Drawing.Color.Black;
+            this.MaximizeBoxC.Location = new System.Drawing.Point(710, 0);
+            this.MaximizeBoxC.Name = "MaximizeBoxC";
+            this.MaximizeBoxC.PressedColor = System.Drawing.Color.Gainsboro;
+            this.MaximizeBoxC.ShadowDecoration.Parent = this.MaximizeBoxC;
+            this.MaximizeBoxC.Size = new System.Drawing.Size(45, 30);
+            this.MaximizeBoxC.TabIndex = 6;
+            // 
+            // TextLabel
+            // 
+            this.TextLabel.AutoSize = true;
+            this.TextLabel.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.TextLabel.Location = new System.Drawing.Point(3, 9);
+            this.TextLabel.Name = "TextLabel";
+            this.TextLabel.Size = new System.Drawing.Size(37, 15);
+            this.TextLabel.TabIndex = 4;
+            this.TextLabel.Text = "{Text}";
+            // 
+            // CloseBoxC
+            // 
+            this.CloseBoxC.Animated = true;
+            this.CloseBoxC.Dock = System.Windows.Forms.DockStyle.Right;
+            this.CloseBoxC.FillColor = System.Drawing.Color.White;
+            this.CloseBoxC.HoverState.Parent = this.CloseBoxC;
+            this.CloseBoxC.IconColor = System.Drawing.Color.Black;
+            this.CloseBoxC.Location = new System.Drawing.Point(755, 0);
+            this.CloseBoxC.Name = "CloseBoxC";
+            this.CloseBoxC.PressedColor = System.Drawing.Color.Gainsboro;
+            this.CloseBoxC.ShadowDecoration.Parent = this.CloseBoxC;
+            this.CloseBoxC.Size = new System.Drawing.Size(45, 30);
+            this.CloseBoxC.TabIndex = 5;
+            // 
+            // FormDrag
+            // 
+            this.FormDrag.TargetControl = this.TopPanel;
+            // 
+            // ipBox
+            // 
+            this.ipBox.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.ipBox.Animated = true;
+            this.ipBox.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.ipBox.DefaultText = "";
+            this.ipBox.DisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
+            this.ipBox.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
+            this.ipBox.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.ipBox.DisabledState.Parent = this.ipBox;
+            this.ipBox.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.ipBox.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.ipBox.FocusedState.Parent = this.ipBox;
+            this.ipBox.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.ipBox.HoverState.Parent = this.ipBox;
+            this.ipBox.Location = new System.Drawing.Point(285, 41);
+            this.ipBox.Name = "ipBox";
+            this.ipBox.PasswordChar = '\0';
+            this.ipBox.PlaceholderText = "";
+            this.ipBox.SelectedText = "";
+            this.ipBox.ShadowDecoration.Parent = this.ipBox;
+            this.ipBox.Size = new System.Drawing.Size(200, 27);
+            this.ipBox.TabIndex = 1;
+            this.ipBox.TextChanged += new System.EventHandler(this.guna2TextBox1_TextChanged);
+            // 
+            // foundBtn
+            // 
+            this.foundBtn.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.foundBtn.Animated = true;
+            this.foundBtn.CheckedState.Parent = this.foundBtn;
+            this.foundBtn.CustomImages.Parent = this.foundBtn;
+            this.foundBtn.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.foundBtn.ForeColor = System.Drawing.Color.White;
+            this.foundBtn.HoverState.Parent = this.foundBtn;
+            this.foundBtn.Location = new System.Drawing.Point(285, 99);
+            this.foundBtn.Name = "foundBtn";
+            this.foundBtn.ShadowDecoration.Parent = this.foundBtn;
+            this.foundBtn.Size = new System.Drawing.Size(200, 35);
+            this.foundBtn.TabIndex = 2;
+            this.foundBtn.Text = "FOUND!";
+            this.foundBtn.Click += new System.EventHandler(this.foundBtn_Click);
+            // 
+            // gMapControl1
+            // 
+            this.gMapControl1.Bearing = 0F;
+            this.gMapControl1.CanDragMap = true;
+            this.gMapControl1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.gMapControl1.EmptyTileColor = System.Drawing.Color.Navy;
+            this.gMapControl1.GrayScaleMode = false;
+            this.gMapControl1.HelperLineOption = GMap.NET.WindowsForms.HelperLineOptions.DontShow;
+            this.gMapControl1.LevelsKeepInMemmory = 5;
+            this.gMapControl1.Location = new System.Drawing.Point(0, 139);
+            this.gMapControl1.MarkersEnabled = true;
+            this.gMapControl1.MaxZoom = 2;
+            this.gMapControl1.MinZoom = 2;
+            this.gMapControl1.MouseWheelZoomEnabled = true;
+            this.gMapControl1.MouseWheelZoomType = GMap.NET.MouseWheelZoomType.MousePositionAndCenter;
+            this.gMapControl1.Name = "gMapControl1";
+            this.gMapControl1.NegativeMode = false;
+            this.gMapControl1.PolygonsEnabled = true;
+            this.gMapControl1.RetryLoadTile = 0;
+            this.gMapControl1.RoutesEnabled = true;
+            this.gMapControl1.ScaleMode = GMap.NET.WindowsForms.ScaleModes.Integer;
+            this.gMapControl1.SelectedAreaFillColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(65)))), ((int)(((byte)(105)))), ((int)(((byte)(225)))));
+            this.gMapControl1.ShowTileGridLines = false;
+            this.gMapControl1.Size = new System.Drawing.Size(800, 311);
+            this.gMapControl1.TabIndex = 3;
+            this.gMapControl1.Zoom = 0D;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ControlBox = false;
+            this.Controls.Add(this.gMapControl1);
+            this.Controls.Add(this.foundBtn);
+            this.Controls.Add(this.ipBox);
+            this.Controls.Add(this.TopPanel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "IP_TmaP";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.TopPanel.ResumeLayout(false);
+            this.TopPanel.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Guna.UI2.WinForms.Guna2Panel TopPanel;
+        private Guna.UI2.WinForms.Guna2ControlBox MinimizeBoxC;
+        private Guna.UI2.WinForms.Guna2ControlBox MaximizeBoxC;
+        private Guna.UI.WinForms.GunaLabel TextLabel;
+        private Guna.UI2.WinForms.Guna2ControlBox CloseBoxC;
+        private Guna.UI2.WinForms.Guna2DragControl FormDrag;
+        private Guna.UI2.WinForms.Guna2TextBox ipBox;
+        private Guna.UI2.WinForms.Guna2Button foundBtn;
+        private GMap.NET.WindowsForms.GMapControl gMapControl1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,93 @@
+﻿using GMap.NET;
+using System;
+using System.Linq;
+using System.Net;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace IP_TmaP
+{
+    public partial class Form1 : Form
+    {
+        private double latitudefalse = 0.0;
+        private double longitudefalse = 0.0;
+        public Form1()
+        {
+            InitializeComponent();
+            TextLabel.Text = Text;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            #region Настройки для компонента GMap
+            gMapControl1.Visible = false;
+            gMapControl1.Bearing = 0;
+            gMapControl1.CanDragMap = true;
+            gMapControl1.DragButton = MouseButtons.Left;
+            gMapControl1.GrayScaleMode = true;
+            gMapControl1.MaxZoom = 17;
+            gMapControl1.MinZoom = 2;
+            gMapControl1.NegativeMode = false;
+            gMapControl1.PolygonsEnabled = true;
+            gMapControl1.ShowTileGridLines = false;
+            gMapControl1.Zoom = 5;
+            gMapControl1.MapProvider = GMap.NET.MapProviders.GMapProviders.GoogleMap;
+            GMaps.Instance.Mode = AccessMode.ServerAndCache;
+            #endregion
+        }
+
+        private void guna2TextBox1_TextChanged(object sender, EventArgs e)
+        {
+            foreach (char line in "1234567890.".ToCharArray())
+            {
+                if (ipBox.Text.Contains(line))
+                {
+
+                    if (!ipBox.Text.Contains('.') && ipBox.Text.Length < 4)
+                    {
+                        ipBox.IconRight = null;
+                    }
+
+                    ipBox.IconRight = null;
+                }
+                else
+                {
+
+                    ipBox.IconRight = Properties.Resources._128px_warning;
+                    if (ipBox.Text == string.Empty)
+                    {
+                        ipBox.IconRight = null;
+                    }
+                }
+            }
+
+        }
+
+        private async void foundBtn_Click(object sender, EventArgs e)
+        {
+            string line = "";
+            using (WebClient wc = new WebClient())
+            {
+                line = wc.DownloadString($"http://ipwhois.app/xml/{ipBox.Text}");
+            }
+
+            gMapControl1.Visible = true;
+            Match match = Regex.Match(line, "<latitude>(.*?)</latitude>(.*?)<longitude>(.*?)</longitude>");
+
+
+
+            string latitudetrue = match.Groups[1].Value.Replace('.', ',');
+            string longitudetrue = match.Groups[3].Value.Replace('.', ',');
+            latitudefalse = double.Parse(latitudetrue);
+            longitudefalse = double.Parse(longitudetrue);
+            TextLabel.Text = latitudetrue + " " + longitudetrue;
+
+
+
+            gMapControl1.Position = new PointLatLng(latitudefalse, longitudefalse);
+            gMapControl1.Zoom = 17;
+        
+
+        }
+    }
+}
